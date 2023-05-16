672a4a502198578d82a9720d51963a5abd185a97
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-05-16 11:54:17 -0300

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Login } from './pages/Login'
+import { Register } from './pages/Register'
+import { Home } from './pages/Home'
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />
+    },
+    {
+      path: "/register",
+      element: <Register />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    }])
+
+  return (
+      <RouterProvider router={router} />
+  )
+}
+
+export default App
