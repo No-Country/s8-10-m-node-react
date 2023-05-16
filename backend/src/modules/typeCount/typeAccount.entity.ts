b3d928b2c7fee252e101a57f3da5a9cca7f5d157
Your Name <you@example.com>
2023-05-16 11:28:22 -0300

    modifique el nombre de la hoja typeAccount

--- backend/src/modules/typeCount/typeAccount.entity.ts
+++ backend/src/modules/typeCount/typeAccount.entity.ts
@@ -2,7 +2,7 @@ import { Column, Entity } from "typeorm";
 import { BaseEntityApp } from "../../shared/entity/baseEntity";
 
 @Entity()
-export class typeCount extends BaseEntityApp {
+export class typeAccount extends BaseEntityApp {
 
     @Column()
     name: string;
