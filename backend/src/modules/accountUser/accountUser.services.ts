ffd17905a4e371523c5939171276bf5d92961052
Luis Mera <luismera86@gmail.com>
2023-05-17 19:17:39 -0300

    Add crud user and accountUser modules

---
+++ backend/src/modules/accountUser/accountUser.services.ts
@@ -0,0 +1,8 @@
+import { BaseServices } from "../../shared/services/baseServices";
+import { AccountUserEntity } from "./accountUser.entity";
+
+export class AccountUserServices extends BaseServices<AccountUserEntity> {
+  constructor() {
+    super(AccountUserEntity);
+  }
+}
---
+++ backend/src/modules/user/user.routes.ts
@@ -0,0 +1,17 @@
+import { BaseRouter } from "../../shared/router/router";
+import { UserController } from "./user.controller";
+import { UserMiddlewares } from "./user.middlewares";
+
+export class UserRouter extends BaseRouter<UserController, UserMiddlewares> {
+  constructor() {
+    super(UserController, UserMiddlewares, "user");
+  }
+
+  routes(path: string): void {
+    this.router.get(`/${path}`, (req, res) => this.controller.getAllController(req, res));
+    this.router.get(`/${path}/:id`, (req, res) => this.controller.getByIdController(req, res));
+    this.router.post(`/${path}`, (req, res) => this.controller.postController(req, res));
+    this.router.put(`/${path}/:id`, (req, res) => this.controller.putController(req, res));
+    this.router.delete(`/${path}/:id`, (req, res) => this.controller.deleteController(req, res));
+  }
+}
