92d62509d6f6cce7fb523f34e502f10c4cea46d8
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-05-18 01:20:48 -0300

    add function to detectCard

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,16 +1,36 @@
 import React from 'react'
 
-export const Card = ({ height, variant, cardNumber }) => {
+export const Card = ({ height, cardNumber }) => {
+  function detectCardType (cardNumber) {
+    const cleanedNumber = cardNumber.replace(/\D/g, '')
+
+    const cardPatterns = {
+      visa: /^4[0-9]{12}(?:[0-9]{3})?$/,
+      masterCard: /^5[1-5][0-9]{14}$/,
+      amex: /^3[47][0-9]{13}$/
+    }
+
+    for (const cardType in cardPatterns) {
+      if (cardPatterns[cardType].test(cleanedNumber)) {
+        return cardType
+      }
+    }
+    return 'nocard'
+  }
+
   function formatCreditCardNumber (number) {
     const parts = number.match(/[\s\S]{1,4}/g)
     return parts.join(' ● ')
   }
+
+  const variant = detectCardType(cardNumber)
 
   const cardHeight = height < 120 ? 120 : height
 
   const width = '340px'
 
   const bgGradient = {
+    nocard: 'bg-gradient-to-r from-red-700 to-red-500',
     visa: 'bg-gradient-to-r from-[#012340] to-[#4148C4]',
     masterCard: 'bg-gradient-to-r from-emerald-500 to-[#0AA46B]',
     amex: 'bg-gradient-to-r from-gray-900 to-slate-800'
@@ -37,7 +57,7 @@ export const Card = ({ height, variant, cardNumber }) => {
       <div className='h-full w-full flex flex-col justify-between p-[20px]'>
         <span className='text-white text-[8px] uppercase flex justify-between px-2'>
           <h3>Credit</h3>
-          <p>Logo</p>
+          <p>{variant}</p>
         </span>
         <span className='text-white uppercase  font-roboto flex flex-col justify-center gap-0 text-xs'>
           <p className='w-full my-0 truncate'>VALENTIN GONZALEZ TRAPAGA</p>
