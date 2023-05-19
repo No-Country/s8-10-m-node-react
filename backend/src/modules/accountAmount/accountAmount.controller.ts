9d1f8242cba09deafbd11c6f4f2549fcea90a458
Bryansss1 <gregoria1540@gmail.com>
2023-05-19 09:12:08 -0400

    controller-services-routes-bryan

---
+++ backend/src/modules/accountAmount/accountAmount.controller.ts
@@ -0,0 +1,74 @@
+import { Request, Response } from "express";
+import { AccountAmountService } from "./accountAmount.services";
+
+
+export class AccountAmountController extends AccountAmountService {
+  constructor() {
+    super();
+  }
+
+  async getAllController(req: Request, res: Response) {
+    try {
+      const result = await this.getServices();
+      res.json({
+        status: "success",
+        response: result,
+      });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async getByIdController(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const result = await this.getServicesById(parseInt(id));
+      res.json({
+        status: "success",
+        response: result,
+      });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async postController(req: Request, res: Response) {
+    const body = req.body;
+    try {
+      const result = await this.postService(body);
+      res.json({
+        status: "success",
+        response: result,
+      });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async putController(req: Request, res: Response) {
+    const { id } = req.params;
+    const body = req.body;
+    try {
+      const result = await this.putService(parseInt(id), body);
+      res.json({
+        status: "success",
+        response: result,
+      });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
+  async deleteController(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const result = await this.deleteService(parseInt(id));
+      res.json({
+        status: "success",
+        response: result,
+      });
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+}
