003491ef15f897dea94370d4a049fe1054661575
Manuel Firman <92244840+manuelfirman@users.noreply.github.com>
2023-05-22 15:08:42 -0300

    AccountCard postController

--- backend/src/modules/accountCard/accountCard.controller.ts
+++ backend/src/modules/accountCard/accountCard.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { AccountCardServices } from "./accountCard.services";
 import { cardUtils } from "./utils/accountCard.utils";
+import { AccountCardEntity } from "./accountCard.entity";
 
 export class AccountCardController extends AccountCardServices {
   constructor() {
@@ -33,15 +34,12 @@ export class AccountCardController extends AccountCardServices {
   }
 
   async postController(req: Request, res: Response) {
-    const numberCard = cardUtils.generateCardNumber();
-    const code = cardUtils.generateCardCvv();
-    const expiration = cardUtils.generateCardExpiration();
-    
-    const body = {
-      numberCard,
-      code,
-      expiration,
-    }
+    const body: AccountCardEntity = new AccountCardEntity();
+    body.cardNumber = cardUtils.generateCardNumber();
+    body.emission = new Date();
+    body.expiration = cardUtils.generateCardExpiration();
+    body.cvv = cardUtils.generateCardCvv();
+    body.accountUser = req.body.accountUser; // ??
 
     try {
       const result = await this.postService(body);
