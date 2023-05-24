df158cb60c5cf63a9dd91762069d52a72cc74ea2
Manuel Firman <92244840+manuelfirman@users.noreply.github.com>
2023-05-24 13:33:51 -0300

    Transaction Fix

--- backend/src/modules/user/user.handler.ts
+++ backend/src/modules/user/user.handler.ts
@@ -1,3 +1,4 @@
+import { EntityManager } from "typeorm";
 import { AppDataSource } from "../../db/postgreSql";
 import { AccountAmountEntity } from "../accountAmount/accountAmount.entity";
 import { accountAmountServices } from "../accountAmount/accountAmount.services";
@@ -21,69 +22,36 @@ class AccountUserHandler {
     try {
       const result = await AppDataSource.manager.transaction(async (transactionalEntityManager) => {
 
-        const userRepository = transactionalEntityManager.getRepository(UserEntity);
-        const accountRepository = transactionalEntityManager.getRepository(AccountUserEntity);
-        const cardRepository = transactionalEntityManager.getRepository(AccountCardEntity);
-        const amountRepository = transactionalEntityManager.getRepository(AccountAmountEntity);
-
         // Create user object
-        const { fullName, lastName, password, email, phone, address, dni, country, postalCode } = body;
-        const newUser = new UserEntity();
-        newUser.fullName = fullName;
-        newUser.lastName = lastName;
-        newUser.address = address;
-        newUser.country = country;
-        newUser.dni = dni;
-        newUser.email = email;
-        newUser.phone = phone;
-        newUser.postalCode = postalCode;
-        newUser.password = await hashPassword.hashPassword(password);
-        const user = await userRepository.save(newUser);
+        const user = await this.createUser(body, transactionalEntityManager);
 
         // Create accountUser
         if(!user) throw new Error("Error: user is null");
-        const newAccountUser = new AccountUserEntity();
-        newAccountUser.user = user;
-        newAccountUser.alias = accountUserUtils.generateAlias(user.fullName, user.lastName, user.dni)
-        newAccountUser.typeCount = "CA";
-        newAccountUser.accountNumber = accountUserUtils.generateAccountNumber();
-        const accountUser = await accountRepository.save(newAccountUser);
+        const accountUser = await this.createAccountUser(user, transactionalEntityManager);
 
-        // Create accountAmount
+        // Create accountAmount and accountCard
         if(!accountUser) throw new Error("Error: accountUser is null");
-        const newAccountAmount = new AccountAmountEntity();
-        newAccountAmount.accountUser = accountUser;
-        newAccountAmount.amount = 0;
-        const currency = await currencyServices.getServicesById(1);
-        if (!currency) throw new Error("Error: currency is null");
-        newAccountAmount.currency = currency;
-        const accountAmount = await amountRepository.save(newAccountAmount);
+        const accountAmount = await this.createAccountAmount(accountUser, transactionalEntityManager);
+        const accountCard = await this.createAccountCard(accountUser, transactionalEntityManager);
 
-        // Create accountCard
-        const newAccountCard = new AccountCardEntity();
-        newAccountCard.cardNumber = cardUtils.generateCardNumber();
-        newAccountCard.expiration = cardUtils.generateCardExpiration();
-        newAccountCard.emission = new Date();
-        newAccountCard.cvv = cardUtils.generateCardCvv();
-        newAccountCard.accountUser = accountUser;
-        const accountCard = await cardRepository.save(newAccountCard);
+        if(!accountAmount || !accountCard) throw new Error("Error: accountAmount or accountCard is null");
 
         return {
           user,
           accountAmount,
           accountCard,
           accountUser
-        }
-      })
+        };
+      });
       return result;
   } catch (error) {
     console.log(error);
   }
   }
 
-  async createUser(body: UserEntity) {
+  async createUser(body: UserEntity, transactionalEntityManager: EntityManager) {
+    const userRepository = transactionalEntityManager.getRepository(UserEntity);
     const { fullName, lastName, password, email, phone, address, dni, country, postalCode } = body;
-    
     const newUser = new UserEntity();
     newUser.fullName = fullName;
     newUser.lastName = lastName;
@@ -95,21 +63,23 @@ class AccountUserHandler {
     newUser.postalCode = postalCode;
     newUser.password = await hashPassword.hashPassword(password);
 
-
-    return newUser;
-  }
-
-  async createAccountUser(user: UserEntity) {
-    const accountUser = new AccountUserEntity();
-    accountUser.user = user;
-    accountUser.alias = accountUserUtils.generateAlias(user.fullName, user.lastName, user.dni)
-    accountUser.typeCount = "CA";
-    accountUser.accountNumber = accountUserUtils.generateAccountNumber();
-    const result = await accountUserServices.postService(accountUser);
+    const result = await userRepository.save(newUser);
     return result;
   }
 
-  async createAccountAmount(accountUser: AccountUserEntity) {
+  async createAccountUser(user: UserEntity, transactionalEntityManager: EntityManager) {
+    const accountRepository = transactionalEntityManager.getRepository(AccountUserEntity);
+    const accountUser = new AccountUserEntity();
+    accountUser.user = user;
+    accountUser.alias = accountUserUtils.generateAlias(user.fullName, user.lastName, user.dni);
+    accountUser.typeCount = "CA";
+    accountUser.accountNumber = accountUserUtils.generateAccountNumber();
+    const result = await accountRepository.save(accountUser);
+    return result;
+  }
+
+  async createAccountAmount(accountUser: AccountUserEntity, transactionalEntityManager: EntityManager) {
+    const amountRepository = transactionalEntityManager.getRepository(AccountAmountEntity);
     const accountAmount = new AccountAmountEntity();
     accountAmount.accountUser = accountUser;
     accountAmount.amount = 0;
@@ -118,18 +88,19 @@ class AccountUserHandler {
     if (!currency) throw new Error("Error: currency is null");
     accountAmount.currency = currency;
 
-    const result = await accountAmountServices.postService(accountAmount);
+    const result = await amountRepository.save(accountAmount);
     return result;
   }
 
-  async createAccountCard(accountUser: AccountUserEntity) {
+  async createAccountCard(accountUser: AccountUserEntity, transactionalEntityManager: EntityManager) {
+    const cardRepository = transactionalEntityManager.getRepository(AccountCardEntity);
     const accountCard = new AccountCardEntity();
     accountCard.cardNumber = cardUtils.generateCardNumber();
     accountCard.expiration = cardUtils.generateCardExpiration();
     accountCard.emission = new Date();
     accountCard.cvv = cardUtils.generateCardCvv();
     accountCard.accountUser = accountUser;
-    const result = await accountCardServices.postService(accountCard);
+    const result = await cardRepository.save(accountCard);
     return result;
   }
 }
