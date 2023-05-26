9a28c84d82459e4e75a167deef5ae51d3458701a
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-05-26 04:34:20 -0300

    refactor router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,8 @@
 import './App.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import { Login } from './pages/Login'
-import { Register } from './pages/Register'
-import { Home } from './pages/Home'
-import { MyCards } from './pages/MyCards'
-import { MyCreditCard } from './pages/MyCreditCard'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
 function App () {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Home />
-    },
-    {
-      path: '/register',
-      element: <Register />
-    },
-    {
-      path: '/login',
-      element: <Login />
-    },
-    {
-      path: '/mycards',
-      element: <MyCards />
-    },
-    {
-      path: '/mycards/:creditCardId',
-      element: <MyCreditCard />
-    }
-  ])
-
   return (
     <RouterProvider router={router} />
   )
