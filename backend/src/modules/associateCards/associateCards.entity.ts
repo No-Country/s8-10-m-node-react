ee8913e096e3d6959e39825e3dd1e4dcec41ee60
Luis Mera <luismera86@gmail.com>
2023-06-01 09:32:35 -0300

    Add middleware checkUser and modify associateCards

--- backend/src/modules/associateCards/associateCards.entity.ts
+++ backend/src/modules/associateCards/associateCards.entity.ts
@@ -1,28 +1,25 @@
 import { Column, Entity, ManyToOne } from "typeorm";
 import { BaseEntityApp } from "../../shared/entity/baseEntity";
 import { AccountUserEntity } from "../accountUser/accountUser.entity";
-
+enum cardType {
+  DEBIT = "DEBIT",
+  CREDIT = "CREDIT",
+}
 @Entity()
 export class AssociateCardsEntity extends BaseEntityApp {
+  
+  @Column({ type: "enum", enum: cardType, default: cardType.DEBIT })
+  type: cardType;
+
   @Column({ unique: true })
   cardNumber: string;
 
   @Column()
-  expiration: Date;
-
-  @Column("timestamp", { nullable: true })
-  emission: Date;
+  cardholder: string;
 
   @Column({ length: 3 })
   cvv: string;
 
-  @Column()
-  emisor: string;
-
-  @Column()
-  bank: string;
-
   @ManyToOne(() => AccountUserEntity, (accountUser) => accountUser.associateCards)
   accountUser: AccountUserEntity;
-  
 }
