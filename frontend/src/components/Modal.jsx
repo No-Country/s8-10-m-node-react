dbcc638bd60c707df93ce69e9f2d36f5072140fe
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-06-04 20:23:58 -0300

    add animation and modal component

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 
-export const Modal = ({ children }) => {
+export const Modal = ({ children, closeModal }) => {
 
   return (
-    <div className='bg-black absolute w-screen h-screen top-0 left-0 z-10 bg-opacity-25 '>
-      <div className='w-[448px] h-[304px] bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-2xl border-2 border-[#4C27AE] p-4 flex flex-col'>
+    <div className='bg-black absolute w-screen h-screen top-0 left-0 z-10 bg-opacity-25' onClick={closeModal} >
+      <div className='sm:w-[448px] sm:min-h-[304px] w-screen bg-white absolute sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 -bottom-4 rounded-2xl border-2 border-[#4C27AE] p-8 flex flex-col sm:animate-slide-in-desktop animate-slide-in-phone' onClick={(e) => e.stopPropagation()}>
         {children}
       </div>
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,6 +9,28 @@ export default {
       fontFamily: {
         roboto: ['Roboto', 'Consolas', 'ui-monospace', 'Menlo'],
         inter: ['Inter', 'Consolas', 'ui-monospace', 'Menlo']
+      },
+      keyframes: {
+        'slide-in-desktop': {
+          '0%': {
+            transform: 'translate(-50%, 100vh)'
+          },
+          '100%': {
+            transform: 'translateY(-50%, -50%)'
+          },
+        },
+        'slide-in-phone': {
+          '0%': {
+            bottom: '-100vh'
+          },
+          '100%': {
+            bottom: '-16px'
+          }
+        }
+      },
+      animation: {
+        'slide-in-desktop': 'slide-in-desktop 0.8s ease-in-out',
+        'slide-in-phone': 'slide-in-phone 0.8s ease-in-out'
       }
     }
   },
