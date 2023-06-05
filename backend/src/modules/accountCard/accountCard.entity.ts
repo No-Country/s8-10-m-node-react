b8b5a548c0afcc9b3def463d2fdb334d9ea4a60b
Luis Mera <luismera86@gmail.com>
2023-06-05 14:20:16 -0300

    Add isActive in AccountCardEntity

--- backend/src/modules/accountCard/accountCard.entity.ts
+++ backend/src/modules/accountCard/accountCard.entity.ts
@@ -4,20 +4,21 @@ import { AccountUserEntity } from "../accountUser/accountUser.entity";
 
 @Entity()
 export class AccountCardEntity extends BaseEntityApp {
-  
-  @Column({unique:true})
+  @Column({ unique: true })
   cardNumber: string;
 
   @Column()
   expiration: Date;
 
+  @Column({ default: true })
+  isActive: boolean;
+
   @Column("timestamp", { nullable: true })
   emission: Date;
 
-  @Column({length: 3})
+  @Column({ length: 3 })
   cvv: string;
 
   @ManyToOne(() => AccountUserEntity, (accountUser) => accountUser.accountCard)
   accountUser: AccountUserEntity;
-  
 }
