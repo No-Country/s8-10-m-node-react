f1a5557bee85a83ca28c75f3a4574348e928668e
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2023-06-07 20:28:42 -0300

    add pagenames on navbar for each page

--- frontend/src/components/PanelBack.jsx
+++ frontend/src/components/PanelBack.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { BackButton } from './BackButton'
+import { useLocation } from 'react-router-dom'
+import { PAGE_NAMES } from '../utils/pageNames'
 
 const PanelBack = ({ name }) => {
+  const location = useLocation()
+  console.log(location)
+  const pageName = location.pathname.split('/').slice(-1)[0]
+  console.log({ pageName })
   return (
     <div className="w-full p-6 bg-white md:bg-transparent flex gap-5 items-center shadow-lg shadow-indigo-500/40 md:shadow-none">
       <BackButton />
-      <h1 className="text-2xl font-bold font-roboto tracking-wide">{name}</h1>
+      <h1 className="text-2xl font-bold font-roboto tracking-wide capitalize">{PAGE_NAMES[pageName]}</h1>
     </div>
   )
 }
---
+++ frontend/src/utils/pageNames.js
@@ -0,0 +1,13 @@
+export const PAGE_NAMES = {
+  home: 'inicio',
+  movements: 'movimientos',
+  transfers: 'transferencias',
+  mycards: 'mis tarjetas',
+  contacts: 'contactos',
+  sendAmount: 'enviar dinero',
+  newContact: 'nuevo contacto',
+  services: 'servicios',
+  options: 'opciones',
+  soporte: 'soporte',
+  perfil: 'perfil'
+}
