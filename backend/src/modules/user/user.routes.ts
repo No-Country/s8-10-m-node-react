89d700952820fa0003591e065f24ed33166688da
Manuel Firman <92244840+manuelfirman@users.noreply.github.com>
2023-06-07 11:01:49 -0300

    get user by email

--- backend/src/modules/user/user.routes.ts
+++ backend/src/modules/user/user.routes.ts
@@ -9,7 +9,7 @@ export class UserRouter extends BaseRouter<UserController, UserMiddlewares> {
 
   routes(path: string): void {
     // this.router.get(`/${path}`, (req, res) => this.controller.getAllController(req, res));
-    this.router.get(`/${path}/:id`, (req, res) => this.controller.getByIdController(req, res));
+    this.router.get(`/${path}`, (req, res) => this.controller.getByEmail(req, res));
     this.router.post(`/${path}`,
       (req, res, next) => this.middleware.checkUser(req, res, next),
       (req, res) => this.controller.postController(req, res));
